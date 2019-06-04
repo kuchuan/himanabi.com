dab142c93459f000ab926a84899625c22142535b
Ryo Horiyama <>
2019-06-04 10:57:23 +0800

    no message

--- Himanabi/resources/views/about.blade.php
+++ Himanabi/resources/views/about.blade.php
@@ -55,16 +55,20 @@
                         <div class="col-md-6">
                             <h4>ABOUT</h4>
                                         <h4 class="subheading"> ひまなび とは？</h4>
-                        <p class="text-muted">aaaaa</p>
+                        <p class="text-muted">私たち「ひまなび」は、セブ島にいる時間を持て余してしまっている人と、<br>
+                          スキルを人並み以上に持っている人々をマッチングさせて、<br>「スキルの共有」「勉強」ができる空間を提供させていただくサービスです.</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <h4>Get hired quickly</h4>
-                                        <h4 class="subheading">GetLance makes it easy to connect with clients and begin doing great work.</h4>
-                                        <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
-                                            Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
-                                            Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+                            <h4></h4>
+                                        <h4 class="subheading">ひまなび の想い</h4>
+                                        <p class="text-muted">いま私たち人々に足りないものは何だと思いますか？<br>
+                                          お喋りできる空間でしょうか？<br>
+                                          私たちはそれを自分自身を共有できる空間だと思います。<br>
+                                          そこには誰かにこの面白みを伝えたい、知らない人に教えてあげたいという貢献、ホスピタリティーと、そこの出会いから生まれるすこしあたたかい時間がそこにはあります。
+
+</p>
                         </div>
                         <div class="col-md-6 how-img">
                             <img src="/img/ノマドワーカー.jpg" class="rounded-circle img-fluid" alt=""/>
@@ -75,20 +79,23 @@
                              <img src="/img/悩む.jpg" class="rounded-circle img-fluid" alt=""/>
                         </div>
                         <div class="col-md-6">
-                            <h4>Work efficiently, effectively.</h4>
-                                        <h4 class="subheading">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
-                                        <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
-                                            Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
-                                            Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+                            <h4></h4>
+                                        <h4 class="subheading">スキルシェアサービス</h4>
+                                        <p class="text-muted">私たちのスキルシェアサービスとは、<br>
+                                          個人のスキル通して、全く知らない人とマッチングし、生涯得た「幸せの作り方」を共有するサービスです。<br>
+                                          <!-- シェアリングエコノミーという言葉が流行語と言っていいほど浸透してきていますが、スキルシェアもその一種になります。<br> -->
+                                          スキマ時間や休みの日に自分のスキルをシェアすることで拡散＝自身のアピールやコネクションを得たりすることができます。<br>
+                                          注目度合いが増してきているスキルシェアサービスで、自身のスキルを他の場所でも生かしていきたいと思っている方は是非参加していください！</p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
-                            <h4>Get paid on time</h4>
-                                        <h4 class="subheading">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
-                                        <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
-                                            Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
-                                            Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
+                            <h4></h4>
+                                        <h4 class="subheading">最後に</h4>
+                                        <p class="text-muted">人と人との出会い方に新しい方法を。<br>
+                                        セブ島にいる皆さんが貴重な時間を無駄にしない為にも、このサービスを通すことでより幸せで、<br>
+                                        より自分の時間と経験をいいものに変換できることを心から望んでいます。<br>
+                                        みなさんの時間、スキル、勉強、貢献を使ってセブ島で新しい発見をしていきましょう。</p>
                         </div>
                         <div class="col-md-6 how-img">
                             <img src="/img/料理.jpg" class="rounded-circle img-fluid" alt=""/>
